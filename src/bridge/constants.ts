6d52d7d93f1b91b1514e8c5bf30964c250cd6028
naftalimurgor <murgornaftali@gmail.com>
2024-06-15 21:53:24 +0300

    add WBGL contract adddresses: etherum and BNB chains

---
+++ src/bridge/constants.ts
@@ -0,0 +1,6 @@
+export const WBGL_CONTRACT_ADDRESS_BNB = '0x2ba64efb7a4ec8983e22a49c81fa216ac33f383a'
+export const WBGL_CONTRACT_ADDRESS_ETH = '0x2ba64efb7a4ec8983e22a49c81fa216ac33f383a'
+
+// @TODO: add Optimims and Arbitrum WBGL Contract addresses:
+export const WBGL_CONTRACT_ADDRESS_ARBITRUM = ''
+export const WBGL_CONTRACT_ADDRESS_OPTIMISM = ''
