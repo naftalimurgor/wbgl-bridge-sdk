6e523a2fea4ebd086ab1246cee82cf3975a5171d
naftalimurgor <murgornaftali@gmail.com>
2024-06-21 09:15:40 +0300

    add swapWBGLtoBGL method #8

--- src/bridge/WBGL.ts
+++ src/bridge/WBGL.ts
@@ -1,10 +1,17 @@
-import { ethers, Wallet } from 'ethers'
+import {
+  ethers,
+  Wallet
+} from 'ethers'
+
 import fetch from 'node-fetch'
-import { IContracts } from '.'
 import WBGL_ABI from '../abi/WBGL'
 import { ChaindIds } from '../chains'
 import { IBridgeConfig } from '../types'
-import { WBGL_CONTRACT_ADDRESS_BNB, WBGL_CONTRACT_ADDRESS_ETH } from './constants'
+
+import {
+  WBGL_CONTRACT_ADDRESS_BNB,
+  WBGL_CONTRACT_ADDRESS_ETH
+} from './constants'
 
 /**
  * @param bglAddress is the address to receive BGL to
@@ -19,14 +26,20 @@ export interface WBGLBGLExchangePair {
   wbglAmount: number
 }
 
+export interface WBGLBGLExchangePairResult {
+  transactionHash: string
+  wbglBalance: string
+  balance: string
+}
+
+type Provider = ethers.providers.JsonRpcProvider | ethers.providers.Web3Provider
+
 export class WBGL {
+
   /**
-   * @member web3 instance to use.
+   * @member A provider instance to use.
    */
-  private readonly provider: ethers.providers.JsonRpcProvider | ethers.providers.Web3Provider
-  /**
-   * @member chainId The chainId of the network to use
-   */
+  private readonly provider: Provider
   private readonly chainId: number | string | ChaindIds
   private readonly chainName: string
   private readonly bridgeEndpoint = 'https://bglswap.com/app/'
@@ -42,17 +55,16 @@ export class WBGL {
   /// START PUBLIC METHODS
   /**
    * @param bglAddress Bitgesell address to receive BGL
-   * @param from
+   * @param wbglAmount amount of WBGL tokens to swap
    * swapWBGLforBGL swaps WBGL for BGL to recepient Bitgesell address
    */
   public async swapWBGLtoBGL({
     bglAddress,
     wbglAmount,
-    sourceWBGLAddress
-  }: WBGLBGLExchangePair) {
+  }: WBGLBGLExchangePair): Promise<WBGLBGLExchangePairResult> {
     try {
       const signer = await this._getSigner()
-      const account = sourceWBGLAddress || await signer.getAddress()
+      const account = await signer.getAddress()
       const signature = await this._signMessage(bglAddress)
 
       const headers = {
@@ -77,7 +89,7 @@ export class WBGL {
 
       const { address: sendAddress } = bridgeResponse
 
-      const txRes = await this._sendWbgl(account, sendAddress, wbglAmount)
+      const txRes = await this._sendWbgl(sendAddress, wbglAmount) as WBGLBGLExchangePairResult
       return txRes
     } catch (error) {
       throw new Error('Failed' + error)
@@ -91,49 +103,59 @@ export class WBGL {
     return signedMessage
   }
 
-
-
   private async _sendWbgl(
-    from: string,
     to: string,
     amount: number
   ) {
+
+    const privateKey = this.evmPrivateKey
+    const signer = new ethers.Wallet(privateKey, this.provider)
+    const recepient = to
+
+    const wbgContractInstance = new ethers.Contract(this._getWBGLTokenAddress(), WBGL_ABI, signer)
+
+    const amountToSend = ethers.utils.parseUnits(String(amount), 18)
+
     try {
-      const txData = {
-        to,
-        gasLimit: null,
-        nonce: null,
-        maxFeePerGas: null,
-        value: null, // 
-        maxPriorityFeePerGas: null
+      const latestFees = await this.provider.getFeeData()
+      const tokenBalance = await wbgContractInstance.balanceOf(signer.address)
+
+      if (tokenBalance.lt(amountToSend)) {
+        throw new Error('Insufficient Token Balance')
       }
 
-      const value = ethers.utils.parseUnits(String(amount), 18)
-      const tokenAddress = this._getWBGLTokenAddress()
-      const signer = await this._getSigner()
-      const nonce = await signer.getTransactionCount()
-      const feeData = await signer.getFeeData()
-      const price = feeData.maxFeePerGas
+      const gasEstimate = await wbgContractInstance.estimateGas.transfer(recepient, amountToSend)
 
-      // TODO: perform gas estimate to avoid exhorbitant gas fees
-      const WBGLContractInstance = new ethers.Contract(tokenAddress, WBGL_ABI)
-      const estimate = WBGLContractInstance.transfer.estimateGas(to, value)
-      const result = await estimate()
+      const maxFeePerGas = latestFees.maxFeePerGas
+      const ethBalance = await this.provider.getBalance(signer.address)
 
-      txData.value = value
-      txData.gasLimit = result
-      txData.nonce = nonce
-      txData.maxFeePerGas = price
-      txData.maxPriorityFeePerGas = price.div(10)
+      // Calculate the total ether required for gas
+      const gasCost = gasEstimate.mul(maxFeePerGas)
 
-      // const fee = price.mul(txData.gasLimit)
+      if (ethBalance.lt(gasCost)) {
+        throw new Error('Insufficient  balance to cover Token Swap')
+      }
 
-      const tx = await WBGLContractInstance.methods.transfer(to, value).send({ from })
-      const request = await WBGLContractInstance.transfer(txData.to, txData.value, tx)
-      return request
+      const tx = await wbgContractInstance.transfer(recepient, amountToSend, {
+        gasLimit: gasEstimate,
+        maxFeePerGas: latestFees.maxFeePerGas,
+        maxPriorityFeePerGas: latestFees.maxPriorityFeePerGas,
+        type: 2
+      })
 
-    } catch (error) {
-      return error
+      const txReceipt = await tx.wait()
+
+      const tokenBalanceAfterSend = await wbgContractInstance.balanceOf(signer.address)
+      const ethBalanceAfterSend = await this.provider.getBalance(signer.address)
+
+      return {
+        transactionHash: txReceipt.transactionHash,
+        wbglBalance: ethers.utils.formatUnits(tokenBalanceAfterSend, 18),
+        balance: ethers.utils.formatEther(ethBalanceAfterSend)
+      }
+
+    } catch (err) {
+      return err
     }
   }
 
@@ -145,7 +167,7 @@ export class WBGL {
 
   // @TODO: include Arbitrum, Optimism checks
   private isChainBsc(chainId: string | number): boolean {
-    const bscChainIds: (string | number)[] = ['0x38', '0x61'] // Binance Smart Chain (Mainnet & Testnet) chain IDs
+    const bscChainIds: (string | number)[] = ['0x38', '0x61'] // BNB Smart Chain (Mainnet & Testnet) chain IDs
     return bscChainIds.includes(chainId)
   }
 
