09c30c036ce5b756791f88c5e996393e43b1b8c2
naftalimurgor <murgornaftali@gmail.com>
2024-06-21 08:07:40 +0300

    refactor supported chain names

--- src/chains.ts
+++ src/chains.ts
@@ -1,14 +1,13 @@
 export enum ChaindIds {
   Ethereum = '0x1',
-  BinanceSmartChain = '0x38',
+  BNBSmartChain = '0x38',
   // ArbitrumOne = '0xa4b1',
   // Optimism = 10
 }
 
 export enum ChainNames {
   Ethereum = 'Ethereum',
-  BinanceSmartChain = 'Binance Smart Chain',
-
+  BNBSmartChain = 'BNBSmartChain',
   // Arbitrum = 'Arbitrum one',
   // Optimism = 'OP Mainnet'
 }
